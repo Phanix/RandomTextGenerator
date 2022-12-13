721b67668a2b681b85c4a05069fc0e0edc96bb4f
Fabio Henrique <fh2game86@gmail.com>
2022-12-13 00:02:47 -0300

    built a function for random words when clicking on text

---
+++ script.js
@@ -0,0 +1,36 @@
+
+let text = document.getElementById("randomText");
+
+text.addEventListener("click", () =>{
+    changeText();
+})
+
+//Word list
+var words = `
+ability,
+able,
+aboriginal,
+abortion,
+about,
+above,
+abraham,
+abroad,
+abs,
+absence,
+absent,
+absolute,
+absolutely,
+absorption,
+abstract,
+abstracts,
+abu,
+abuse,
+`;
+var wordsArray = words.split(",");
+
+function changeText(){
+    text.innerHTML = wordsArray[Math.floor(Math.random() *  wordsArray.length)];
+}
+
+
+
